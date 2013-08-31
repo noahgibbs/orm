84657cd7e7fa3fec66efe0c6d1d2f4ef4927962c
Noah Gibbs <noah_gibbs@yahoo.com>
2013-08-31 16:29:57 -0700

    Debug, add tests.

--- lib/orm/model.rb
+++ lib/orm/model.rb
@@ -1,20 +1,79 @@
 require "sqlite3"
 
-DB = SQLite3::Database.new "testdb1"
+DB = SQLite3::Database.new "test1.db"
 
-module ORM
-  class Model
-    def self.table_name
-      name.downcase  # lowercased *class* name
+module ORM; class Model
+  def self.schema
+    return @schema if @schema
+    table_info = DB.table_info(name.downcase)
+    @schema = {}
+    table_info.each do |row|
+      @schema[row["name"]] = row["type"]
     end
-
-    def self.schema
-      return @schema if @schema
-      @schema = {}
-      DB.table_info(table_name) do |row|
-        @schema[row["name"]] = row["type"]
-      end
-      @schema
-    end
+    @schema
   end
-end
+
+  def self.table
+    name.downcase
+  end
+
+  def self.to_sql(value)
+    return "null" if value.nil?
+    return value.to_s if value.is_a?(Numeric)
+    value.inspect
+  end
+
+  def self.insert(vals)
+    k = schema.keys
+    v = k.map { |key| to_sql(vals[key]) }
+
+    DB.execute <<-SQL
+      INSERT INTO #{name.downcase}
+      (#{k.join ","})
+      VALUES (#{v.join ","});
+    SQL
+  end
+
+  def initialize(data = nil)
+    @hash = data
+  end
+
+  def self.find(id)
+    row = DB.execute <<-SQL
+      select #{schema.keys.join ","} from #{table}
+      where id = #{id};
+    SQL
+    data = Hash[schema.keys.zip row[0]]
+    self.new data
+  end
+
+  def [](name)
+    @hash[name]
+  end
+
+  def []=(name, value)
+    @hash[name] = value
+  end
+
+  def update!
+    fields = @hash.map do |k, v|
+      "#{k}=#{self.class.to_sql(v)}"
+    end
+
+    DB.execute <<-SQL
+      UPDATE #{self.class.table}
+      SET #{fields.join ","}
+      WHERE id = #{@hash["id"]}
+    SQL
+  end
+
+  def self.create(values)
+    values.delete "id"
+    insert values
+
+    sql = "SELECT last_insert_rowid()"
+    new_id = DB.execute(sql)[0][0]
+    self.find new_id
+  end
+
+end; end
--- test/test_orm.rb
+++ test/test_orm.rb
@@ -9,27 +9,81 @@ end
 
 class TestORM < MiniTest::Unit::TestCase
   def setup
-    @db = SQLite3::Database.new "unittest.db"
+    @db = SQLite3::Database.new "test1.db"
     schema = @db.table_info("posts")
     if schema.nil? || schema.empty?
       # Create posts table
       @db.execute <<-SQL
         CREATE TABLE posts (
+          id integer primary key,
           title varchar(64),
           body varchar(128),
-          rating smallint
+          rating integer
         );
       SQL
     end
 
+    # And make sure posts is empty
     @db.execute "DELETE FROM posts;"
   end
 
   def test_orm
-    assert_equal ["body", "rating", "title"], Posts.schema.keys.sort
+    assert_equal ["body", "id", "rating", "title"], Posts.schema.keys.sort
   end
 
-  def test_truth
-    assert true
+  def test_insert
+    Posts.insert "title" => "Post 1", "rating" => 3
+
+    row = @db.execute <<-SQL
+      select title, rating from posts;
+    SQL
+
+    assert_equal row[0][0], "Post 1"
+    assert_equal row[0][1], 3
+  end
+
+  def test_find
+    Posts.insert "id" => 7, "title" => "Yup"
+
+    p = Posts.find(7)
+    assert_equal p["title"], "Yup"
+  end
+
+  def test_update
+    Posts.insert "id" => 9, "title" => "T1", "rating" => 4
+    Posts.insert "id" => 1, "title" => "T2", "rating" => 6
+
+    p = Posts.find(9)
+    p["rating"] = 3
+    p.update!
+
+    p = Posts.find(1)
+    p["title"] = "T3"
+    p.update!
+
+    row = @db.execute <<-SQL
+      select id, title, rating from posts where id = 9;
+    SQL
+    assert_equal [9, "T1", 3], row[0]
+
+    row = @db.execute <<-SQL
+      select id, title, rating from posts where id = 1;
+    SQL
+    assert_equal [1, "T3", 6], row[0]
+  end
+
+  def test_create
+    Posts.insert "title" => "T1", "rating" => 7
+
+    p = Posts.create "title" => "T2", "rating" => 5
+
+    row = @db.execute <<-SQL
+      select title from posts;
+    SQL
+
+    assert_equal ["T1", "T2"], [row[0][0], row[1][0]]
+
+    assert_equal "T2", p["title"]
+    assert_equal 5, p["rating"]
   end
 end
